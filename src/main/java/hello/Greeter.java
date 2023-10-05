6d726675ddf88291be9ed740157c0339207f68eb
Steve Hickman <steve@epistimis.com>
2023-10-05 09:34:37 -0700

    Updated doc

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,7 +1,16 @@
 package hello;
 
+/**
+ * The `Greeter` class is a simple class that provides a method for generating a greeting message.
+ */
 public class Greeter {
+  
+  /**
+   * Returns the string "Hello to the world!".
+   * 
+   * @return the greeting message
+   */
   public String sayHello() {
-    return "Hello world!";
+    return "Hello to the world!";
   }
 }
